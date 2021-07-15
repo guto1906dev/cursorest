a6d6ee762a4a36ce8009f849e2871d7238037698
guto1906dev <guto1906@yahoo.com.br>
2021-07-15 12:48:19 -0300

    Project Created

---
+++ src/main/java/com/guto1906/cursorest/CursorestApplication.java
@@ -0,0 +1,13 @@
+package com.guto1906.cursorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guto1906/cursorest/CursorestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guto1906.cursorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
