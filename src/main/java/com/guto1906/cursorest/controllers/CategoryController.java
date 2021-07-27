09f3b2a56e9ac4fa60f4cdf1a661e399548c2aea
guto1906dev <guto1906@yahoo.com.br>
2021-07-26 22:05:51 -0300

    PUT, DELETE e excessao personalizada para Delete

--- src/main/java/com/guto1906/cursorest/controllers/CategoryController.java
+++ src/main/java/com/guto1906/cursorest/controllers/CategoryController.java
@@ -1,13 +1,19 @@
 package com.guto1906.cursorest.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guto1906.cursorest.dto.CategoryDto;
 import com.guto1906.cursorest.services.CategoryService;
@@ -35,5 +41,27 @@ public class CategoryController {
 		return ResponseEntity.ok().body(entity);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody CategoryDto dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@RequestBody CategoryDto dto, @PathVariable(value = "id") Long id){
+		dto.setId(id);
+		dto = service.update(dto);
+				
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id){
+		service.delete(id);				
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- src/main/java/com/guto1906/cursorest/services/CategoryService.java
+++ src/main/java/com/guto1906/cursorest/services/CategoryService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import com.guto1906.cursorest.domain.Category;
 import com.guto1906.cursorest.dto.CategoryDto;
 import com.guto1906.cursorest.repositories.CategoryRepository;
+import com.guto1906.cursorest.services.exceptions.DataIntegrityException;
 import com.guto1906.cursorest.services.exceptions.NoSuchElementException;
 
 @Service
@@ -37,5 +38,32 @@ public class CategoryService {
 		
 		return dto;
 	}
+	
+	public CategoryDto insert(CategoryDto dto) {
+		Category entity = new Category(null, dto.getName());
+		return new CategoryDto(repo.save(entity));		
+	}
+	
+	@Transactional
+	public CategoryDto update(CategoryDto dto) {
+		findById(dto.getId());
+		Category entity = new Category(dto.getId(), dto.getName());
+		return new CategoryDto(repo.save(entity));		
+	}
+	
+	@Transactional
+	public void delete(Long id) {
+		CategoryDto dto = findById(id);
+		
+		if(dto.getProducts().isEmpty()) {
+			repo.deleteById(id);
+		} else {
+			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos associados.");
+		}
+		
+		
+		
+	}
+	
 
 }
